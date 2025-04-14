73821d99496a42f1d008bee4222df74f9af365d6
SASASHHJ <jabeur.amir@esprit.tn>
2025-04-15 01:12:38 +0200

    commit

--- src/Entity/Activities.php
+++ src/Entity/Activities.php
@@ -1,16 +1,12 @@
 <?php
 
-namespace App\Entity; 
+namespace App\Entity;
 
 
 use App\Repository\ActivitiesRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
-
-
-
-
 
 #[ORM\Entity(repositoryClass: ActivitiesRepository::class)]
 #[ORM\Table(name: "activities")]
